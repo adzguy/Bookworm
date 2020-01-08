0b82686488c00f9ce42640bbd315bf35aa4551b7
Davron <devrannur@gmail.comn>
2020-01-08 00:22:44 -0600

    AnyView wrapper

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -8,32 +8,24 @@
 
 import SwiftUI
 
-struct PushButton: View {
-    let title: String
-    @Binding var isOn: Bool
-    var onColor = [Color.red, Color.blue]
-    var offColor = [Color(white: 0.6), Color(white: 0.4)]
-    
-    var body: some View {
-        Button(title) {
-            self.isOn.toggle()
-        }
-        .padding()
-        .foregroundColor(.white)
-        .background(LinearGradient(gradient: Gradient(colors: isOn ? onColor : offColor), startPoint: .top, endPoint: .bottom))
-        .clipShape(Capsule())
-        .shadow(radius: isOn ? 0 : 5)
-        
-    }
-}
 struct ContentView: View {
     
-    @State private var rememberMe = false
-    
+    @Environment(\.horizontalSizeClass) var sizeClass
+
     var body: some View {
-        VStack {
-            PushButton(title: "Push Button", isOn: $rememberMe)
-            Text(rememberMe ? "On" : "Off")
+        if sizeClass == .compact {
+            return AnyView(VStack {
+                Text("Active size class:")
+                Text("COMPACT")
+            }
+            .font(.largeTitle))
+        }
+        else {
+            return AnyView(HStack {
+                Text("Active size class:")
+                Text("REGULAR")
+            }
+            .font(.largeTitle))
         }
     }
 }
