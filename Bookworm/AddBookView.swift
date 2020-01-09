66396e2e075a15db816afd26418c381e5406e842
Davron <devrannur@gmail.comn>
2020-01-09 03:43:54 -0600

    DetailView complete. EditButton added. Swipe delete added.

--- Bookworm/AddBookView.swift
+++ Bookworm/AddBookView.swift
@@ -17,7 +17,7 @@ struct AddBookView: View {
     @State private var rating = 3
     @State private var genre = ""
     
-    let genres = ["Fantacy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller"]
+    let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romantic", "Thriller"]
     var body: some View {
         NavigationView {
             Form {
---
+++ Bookworm/DetailView.swift
@@ -0,0 +1,65 @@
+//
+//  DetailView.swift
+//  Bookworm
+//
+//  Created by Davron on 1/8/20.
+//  Copyright © 2020 Davron. All rights reserved.
+//
+
+import SwiftUI
+import CoreData
+
+struct DetailView: View {
+    
+    let book: Book
+    
+    var body: some View {
+        GeometryReader { geo in
+            VStack {
+                ZStack(alignment: .bottomTrailing) {
+                    Image(self.book.genre ?? "Fantasy")
+                        .frame(maxWidth: geo.size.width)
+                    
+                    Text(self.book.genre?.uppercased() ?? "FANTASY")
+                        .font(.caption)
+                        .fontWeight(.black)
+                        .padding(8)
+                        .foregroundColor(.white)
+                        .background(Color.black.opacity(0.75))
+                        .clipShape(Capsule())
+                        .offset(x: -5, y: -5)
+                }
+                Text(self.book.author ?? "Unknown author")
+                    .font(.title)
+                    .foregroundColor(.secondary)
+                
+                Text(self.book.review ?? "No review")
+                    .padding()
+                
+                RatingView(rating: .constant(Int(self.book.rating)))
+                    .font(.largeTitle)
+                
+                Spacer()
+            }
+        }
+        //.navigationBarTitle(Text(book.title) ?? "Unkown Book", displayMode: .inline)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static let moc = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+    
+    static var previews: some View {
+        let book = Book(context: moc)
+        book.title = "Test Book"
+        book.author = "Test author"
+        book.genre = "Fantacy"
+        book.rating = 4
+        book.review = "This was a great book; I really enjoyed it"
+        
+        return NavigationView {
+            DetailView(book: book)
+        }
+        
+    }
+}
